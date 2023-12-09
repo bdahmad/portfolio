c7c0d15da95784092564a30905d2165018c6a6dc
Ahmad Ali <ahmadali09787@gmail.com>
2023-12-10 00:20:46 +0600

    separate js file

---
+++ public/asset/js/costom.js
@@ -0,0 +1,7 @@
+   
+   
+    let name = document.getElementsByClassName("name");
+    let email = document.getElementsByClassName("email");
+    let subject = document.getElementsByClassName("subject");
+    let message = document.getElementsByClassName("message");
+    let btnSend = document.getElementsByClassName("btnSend");
